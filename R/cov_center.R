69e58e520a070891a163ee2004b8562092e806f7
luismurao <luismurao@gmail.com>
2017-03-21 17:06:51 -0600

    Ellipsoid volume level

--- R/cov_center.R
+++ R/cov_center.R
@@ -32,24 +32,24 @@
 #' ## Data input
 #' # head(covar_centroid$data)
 cov_center <- function(data,mve=TRUE,level,vars=NULL){
-  
+
   if(is.null(vars)){
-    
+
     nvars <- readline(prompt="Number of variables to fit the ellipsoid model:\n\n")
     data <- data.frame(data)
-    
+
     allvars <- names(data)
     print(nvars)
     vars <- numeric(nvars)
     cat('Select a variable form the list:\n\n')
     for (i in 1:dim(data)[2]){
       cat(i,'.-',allvars[i],'\n')
-      
+
     }
     cont <- 1
     while(0 %in% vars){
       n <- readline(prompt="Enter an option from the above list: ")
-      
+
       if(n %in% 1:dim(data)[2]){
         vars[cont] <- as.numeric(n)
         cont <- cont+1
@@ -57,13 +57,13 @@ cov_center <- function(data,mve=TRUE,level,vars=NULL){
       else{
         cat('Option not in the list:\n')
       }
-      
+
     }
   }
   data <- data[,vars]
-  
+
   if(mve){
-    
+
     # Compute the number of points of our data that represent the proportion of the data
     NDquntil <- function(nD,level){
       n <- round(nD*level)
@@ -71,7 +71,7 @@ cov_center <- function(data,mve=TRUE,level,vars=NULL){
         n <- nD
       return(n)
     }
-    
+
     n <-NDquntil(dim(data)[1],level)
     # Centroid and covarianve for the Minimum volume Ellipsoid
     cent_var <- cov.mve(data,quantile.used =n)
@@ -82,28 +82,28 @@ cov_center <- function(data,mve=TRUE,level,vars=NULL){
     centroid <- colMeans(data)
     vari <- cov(data)
   }
-  
+
   # Axis length computations
-  sigmaI <- solve(vari)/qchisq(0.99,df = dim(data)[2])
+  sigmaI <- solve(vari)/qchisq(level,df = dim(data)[2])
   sigIEigenSys <- eigen(sigmaI)
   sigIEval <- sigIEigenSys$values
   sigIEvec <- sigIEigenSys$vectors
-  
+
   stds <- 1/sqrt(sigIEval)
-  
+
   axis_length <- list()
   for(i in 1:dim(sigmaI)[1]){
     assign(paste0(letters[i]), stds[i]*2)
-    axis_length[[i]] <- eval(parse(text=letters[i])) 
+    axis_length[[i]] <- eval(parse(text=letters[i]))
   }
-  
+
   names(axis_length) <- letters[1:dim(vari)[1]]
   axis_length <- unlist(axis_length)
-  
+
   n <- dim(vari)[1]
-  
+
   # Axis volume
-  
+
   ellip_vol <- function(n,axis_length){
     term1 <- 2* pi^(n/2)
     term2 <- n*gamma(n/2)
@@ -112,32 +112,32 @@ cov_center <- function(data,mve=TRUE,level,vars=NULL){
     return(term4)
   }
   vol2 <- ellip_vol(n,axis_length/2)
-  
-  
+
+
   # Semi-axis orientation
-  
+
   axis_coordinates <- list()
-  
+
   for(i in 1:dim(vari)[1]){
     assign(paste0("l",i,"_inf"),
            centroid - sigIEvec[,i]*stds[i])
     assign(paste0("l",i,"_sup"),
            centroid + sigIEvec[,i]*stds[i])
-    
+
     coord_matrix <- matrix(c(eval(parse(text=paste0("l",i,"_sup"))),
                              eval(parse(text=paste0("l",i,"_inf")))
     ),byrow = T,nrow = 2)
     colnames(coord_matrix) <- names(centroid)
     rownames(coord_matrix) <- paste0("vec_",1:2)
     axis_coordinates[[i]] <- coord_matrix
-    
+
   }
-  
-  
-  
+
+
+
   return(list(centroid=centroid,covariance=vari,
               niche_volume =vol2,
               SemiAxis_length=axis_length/2,
               axis_coordinates=axis_coordinates))
-  
+
 }
