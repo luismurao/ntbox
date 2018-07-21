f8ee4d02e5d26aabcf017f2434e558fe34e2bf31
luismurao <luismurao@gmail.com>
2018-07-21 17:36:26 -0500

    Check package dependencies

--- R/run_ntbox.R
+++ R/run_ntbox.R
@@ -5,6 +5,13 @@
 #' # Run nichetoolbox package
 #' # run_nichetoobox()
 run_ntbox <- function(){
+
+  to_install <- system.file("shinyApp/load_install_pkgs.R",
+                            package = "ntbox")
+  cat("Looking for additiona nichetoolbox dependencies")
+  cat("\nNote that the first time can take few time\n")
+  source(to_install)
+
   app_path <- system.file("shinyApp",package = "ntbox")
   return(shiny::runApp(app_path))
 }
--- inst/shinyApp/load_install_pkgs.R
+++ inst/shinyApp/load_install_pkgs.R
@@ -1,3 +1,22 @@
+pkg_check <- c("shiny","rgeos","rgdal",
+               "raster","maptools","dismo",
+               "rgl","dygraphs","dygraphs",
+               "png","XML","rmarkdown",
+               "knitr","stringr","stringr",
+               "MASS","animation","mgcv",
+               "googleVis","rasterVis","plyr",
+               "shinyBS","shinyjs","rglwidget",
+               "car","maps","corrplot",
+               "dplyr","cluster","sqldf",
+               "fields","devtools","psych",
+               "magrittr","shinythemes","grid",
+               "RColorBrewer","ade4","V8","spocc")
+
+pkgs_ntb_miss <- pkg_check[!(pkg_check %in% installed.packages())]
+if(length(pkgs_ntb_miss)>=0L){
+  install.packages(pkgs_ntb_miss,repos = "https://cloud.r-project.org/")
+}
+
 # Github dependencies
 devtools::install_github("luismurao/leaflet")
 
@@ -9,11 +28,12 @@ pkgs_ntb_miss <- pkgs_ntb[!(pkgs_ntb %in% installed.packages())]
 if(length(pkgs_ntb_miss)>0L){
   devtools::install_github("AnalytixWare/ShinySky")
   devtools::install_github("ENMGadgets", "narayanibarve")
-
 }
 library(ENMGadgets)
 library(shinysky)
 library(leaflet)
+sapply(pkgs_ntb_miss,function(x) library(x,character.only = TRUE))
+
 # Load packages
 options(rgl.useNULL=TRUE)
 #sapply(pkgs_ntb,function(x) library(x,character.only = TRUE))
