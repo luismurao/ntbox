820a7083231ee291f43f7dd42fe2dfb0d8cd3d47
luismurao <luismurao@gmail.com>
2018-07-23 13:58:47 -0500

    V 0.2.5.1 Function to stop cluster and realese RAM when multisession plan is activated on future (windows users)

---
+++ inst/shinyApp/helpers/ClusterRegistry1.R
@@ -0,0 +1,50 @@
+#' The function is take from the future package, for details visit:
+#' https://github.com/HenrikBengtsson/future
+ClusterRegistry1 <- local({
+  last <- NULL
+  cluster <- NULL
+  function(action = c("get", "start", "stop"), workers = NULL, ...) {
+    action <- match.arg(action)
+
+    if (is.null(workers)) {
+    } else if (is.numeric(workers)) {
+      workers <- as.integer(workers)
+      stop_if_not(length(workers) == 1, is.finite(workers))
+    } else if (is.character(workers)) {
+      stop_if_not(length(workers) >= 1, !anyNA(workers))
+      workers <- sort(workers)
+    } else {
+      stop("Unknown mode of argument 'workers': ", mode(workers))
+    }
+
+    if (length(cluster) == 0L && action != "stop") {
+      cluster <<- .makeCluster(workers, ...)
+      last <<- workers
+    }
+
+    if (action == "get") {
+      return(cluster)
+    } else if (action == "start") {
+      ## Already setup?
+      if (!identical(workers, last)) {
+        ClusterRegistry(action = "stop")
+        cluster <<- .makeCluster(workers, ...)
+        last <<- workers
+      }
+    } else if (action == "stop") {
+      if (length(cluster) > 0L) try(stopCluster(cluster), silent = TRUE)
+      cluster <<- NULL
+      last <<- NULL
+    }
+
+    invisible(cluster)
+  }
+}) ## ClusterRegistry()
+
+
+.makeCluster <- function(workers, ...) {
+  if (length(workers) == 0L) return(NULL)
+  makeClusterPSOCK(workers, ...)
+} ## .makeCluster()
+
+
