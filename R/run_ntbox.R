a1e267bbb9ba6035c91b35806ffedf1d66b8da8f
luismurao <luismurao@gmail.com>
2018-07-22 23:58:11 -0500

    V. 0.2.5.1 RTools check

--- R/run_ntbox.R
+++ R/run_ntbox.R
@@ -11,11 +11,22 @@ run_ntbox <- function(){
 
   cat("\n\n")
   cat("|----------------------------------------------------------------|\n")
-  cat("|      **** Welcome to nichetoolbox version", paste0(packageVersion("ntbox")," ****         |\n"))
+  cat("|      **** Welcome to nichetoolbox version", paste0(utils::packageVersion("ntbox"),
+                                                            " ****         |\n"))
   cat("|----------------------------------------------------------------|\n")
   cat("|           Looking for additional nichetoolbox dependencies     |\n")
   cat("|           Note that the first time can take few time           |\n")
   cat("|----------------------------------------------------------------|\n\n\n")
+
+  if(.Platform$OS.type != "unix"){
+    rtools_inst <- devtools::find_rtools()
+    if(!rtools_inst){
+      cat("If you have troubles while running ntbox please install Rtools:\n\n")
+      cat("https://cran.r-project.org/bin/windows/Rtools/")
+
+    }
+
+  }
 
   source(to_install)
 
