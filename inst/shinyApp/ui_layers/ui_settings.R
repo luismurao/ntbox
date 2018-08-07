eb205849106380c52f3d84271356a71b6dbbd870
luismurao <luismurao@gmail.com>
2018-08-06 20:40:23 -0500

    V. 0.3.0.0 load ui layers as a function of OS

--- inst/shinyApp/ui_layers/ui_settings.R
+++ inst/shinyApp/ui_layers/ui_settings.R
@@ -1,4 +1,5 @@
 # App Settings & Materials (tutorials and intro to the app)
+osSystem <- Sys.info()["sysname"]
 
 materials <- sidebarLayout(position = "left",
                            sidebarPanel(
@@ -18,19 +19,29 @@ materials <- sidebarLayout(position = "left",
                              h4("Niche layers"),
                              p("One ne of the most important things for your data analysis in NTB is to select the folder with the raster layer that you will use as niche variables."),
                              br(),
-                             p("Please select the folder of raster layers and remember that all of them need to be in the same resolution and extension"),
+                             p("Please select the folder of raster layers; notice that all of them need to be in the same resolution and extension"),
+                             p("The supported raster formats are: ",".asc",
+                               ".bil",",",".tif",",",".nc.",
+                               ",",".sdat",",",".img"),
                              br(),
                              #directoryInput('ras_layers_directory', label = 'Select a raster layers directory'),
                              #glyphicon glyphicon-folder-open
-                             shinyFiles::shinyDirButton(id = "ras_layers_directory",
-                                                        label =  "Select raster layers directory",
-                                                        title = "Select a directory from the directories panel",
-                                                        icon = icon("folder-open",
-                                                                    lib = "glyphicon"),
-                                                        buttonType = "info"),
-                             br(),
-                             HTML(paste('<div class="alert alert-info" role="alert">',
-                                  verbatimTextOutput("layers_directory"),'</div>')),
+                             conditionalPanel(paste0("'",osSystem,"' != 'Darwin'"),
+                                              directoryInput('ras_layers_directory', label = 'Select a raster layers directory')
+                                              ),
+                             conditionalPanel(paste0("'",osSystem,"' == 'Darwin'"),
+                                              shinyFiles::shinyDirButton(id = "ras_layers_directory",
+                                                                         label =  "Select raster layers directory",
+                                                                         title = "Select a directory from the directories panel",
+                                                                         icon = icon("folder-open",
+                                                                                     lib = "glyphicon"),
+                                                                         buttonType = "info"),
+                                              br(),
+                                              HTML(paste('<div class="alert alert-info" role="alert">',
+                                                         verbatimTextOutput("layers_directory"),'</div>'))
+                                              ),
+
+
                              #shiny::selectInput(inputId = )
                              #selectInput(inputId = 'res',label = 'Select a grid resolution for the bioclimatic vars',choices = c('10 Arc-minutes'='tenArcs','5 Arc-minutes'='fiveArcs','2.5 Arc-minutes'='twoArcs'),selected = 'tenArcs'),
                              bsButton("loadNicheLayers","Load niche layers",icon = icon("upload", lib = "glyphicon"),style = "primary"),
@@ -45,27 +56,28 @@ materials <- sidebarLayout(position = "left",
                              #shinyDirButton('worflowPath', 'Folder select', 'Please select a folder'),
                              #shinyDirButton("workflowPath",title = "Choose Download Directory",
                              #              label = "Browse Download Directory ", class = "btn-block btn-link"),
-                            # directoryInput('wf_directory', label = 'Select workflow directory'),
-                            shinyFiles::shinyDirButton(id = "wf_directory",
-                                                       label =  "Select workflow directory",
-                                                       title = "Select a directory from the directories panel",
-                                                       icon = icon("folder-open",
-                                                                   lib = "glyphicon"),
-                                                       buttonType = "info"),
-                            br(),
-                            HTML(paste('<div class="alert alert-info" role="alert">',
-                                       verbatimTextOutput("work_directory"),'</div>'))
 
-                               #submitButton()
+                             conditionalPanel(paste0("'",osSystem,"' != 'Darwin'"),
+                                              directoryInput('wf_directory', label = 'Select workflow directory')),
+                             conditionalPanel(paste0("'",osSystem,"' == 'Darwin'"),
+                                              shinyFiles::shinyDirButton(id = "wf_directory",
+                                                                         label =  "Select workflow directory",
+                                                                         title = "Select a directory from the directories panel",
+                                                                         icon = icon("folder-open",
+                                                                                     lib = "glyphicon"),
+                                                                         buttonType = "info"),
+                                              br(),
+                                              HTML(paste('<div class="alert alert-info" role="alert">',
+                                                         verbatimTextOutput("work_directory"),'</div>'))
+                             )
 
+                             #submitButton()
 
                              #h4('Tutorials'),
                              #HTML('In this part you can find some tutorials that show how to use the app.'),
                              #selectInput(inputId = 'tutorialsm',label = 'Select one of the turtorials',
                              #            choices =  c('About'='about1','1. Data'='datose'),
                               #           selected = "about1",multiple = FALSE)
-
-
                                ),
                            mainPanel(
                              #htmlOutput("page"),
