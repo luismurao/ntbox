d6781add5ffa29697d92cc9d4f2241cbafab1533
luismurao <luismurao@gmail.com>
2018-08-06 20:37:38 -0500

    V. 0.3.0.0 Added trapezoidal integration using function written in Rcpp

---
+++ R/trapezoid_roc.R
@@ -0,0 +1,11 @@
+#' Trapezoidal integration for Partial ROC curve
+#' @param fract_area fractional area predicted presence
+#' @param sensibility sensibility at threshold (fract_area value).
+#' @description trapozoid_roc applies the trapezoidal integration for Partial ROC curve.
+#' @return Returns the area under the Partial ROC curve.
+#' @useDynLib ntbox
+#' @export
+
+trapozoid_roc <- function(fract_area,sensibility){
+  return(trap_roc(fract_area,sensibility))
+}
