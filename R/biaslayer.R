7b1f8ba4233560ddd76e74a9d6464bf2c5560466
luismurao <luismurao@gmail.com>
2018-12-20 19:36:50 -0600

    Create biaslayer.R
    V 0.4.1.2 function to compute a biaslayer

---
+++ R/biaslayer.R
@@ -0,0 +1,39 @@
+#' biaslayer: Function to create a bias layer
+#'
+#' @description biaslayer estimates a raster that represents the sampling bias; it can be used in the \code{\link{maxent_call}{ntbox}} function.
+#' @param occs_df A data.frame with the occurrence data and coordinates.
+#' @param longitude A character vector of the column name of longitude.
+#' @param latitude A character vector of the column name of latitude.
+#' @param raster_mold A raster of the calibration area. This will serve just as a mold to make the bias layer.
+#' @export
+#' @details The biaslayer uses the 2 dimensional kernel desity estimator \code{\link{kde2d}{MASS}}.
+#' @examples
+#' # Birds of North America
+#' birds <- read.csv(system.file("extdata",
+#'                               "birdsN_America.csv",
+#'                               package = "ntbox"))
+#' bio01 <- raster::raster(system.file("extdata",
+#'                                     "bio01_bias.tif",
+#'                                     package = "ntbox"))
+#'
+#' biasBirds <- biaslayer(occs_df = birds,
+#'                        longitude = "Longitude",
+#'                        latitude = "Latitude",
+#'                        raster_mold = bio01)
+#'
+#' raster::plot(biasBirds)
+#'
+biaslayer <- function(occs_df,longitude, latitude, raster_mold){
+  ll_ras <- raster::rasterize(occs_df[,c(longitude,latitude)],
+                              raster_mold, 1)
+  ll_rasIDs <- which(raster::getValues(ll_ras) %in% 1)
+  occ_T <- sp::coordinates(ll_ras)[ll_rasIDs,]
+
+  dens <- MASS::kde2d(occ_T[,1], occ_T[,2],
+                      n = c(nrow(ll_ras),
+                            ncol(ll_ras)))
+  biasLayer <- raster::raster(dens)
+  if(!all(dim(biasLayer)[1:2] == dim(raster_mold)[1:2]))
+    biasLayer <- raster::resample(biasLayer,raster_mold)
+  return(biasLayer)
+}
