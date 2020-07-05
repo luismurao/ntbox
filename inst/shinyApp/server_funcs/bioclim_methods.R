6e8eeb5b359622417b611b3323eea19fa2097638
Osorio Olvera <l916o895@home.ku.edu>
2020-07-05 15:18:55 -0500

    Data partion for train and test data

--- inst/shinyApp/server_funcs/bioclim_methods.R
+++ inst/shinyApp/server_funcs/bioclim_methods.R
@@ -11,6 +11,21 @@ observe({
 })
 
 
+
+
+output$brand <- renderUI({
+  if(!is.null(data_partition()$type)){
+    checkboxInput("bpartition","Use train data generated in ntbox to fit the model",
+                  value = TRUE)
+  }
+  #else{
+  #  numericInput("ertestprop","Train proportion",0.7,min = 0.01,
+  #               max=0.99)
+  #  actionButton("run_part","Go!!!",styleclass = "primary")
+  #}
+
+})
+
 # ---------------------------------------------------------------------
 # Fit Bioclim model
 # ---------------------------------------------------------------------
@@ -19,7 +34,13 @@ observe({
 bioclim_model_all_all_train <- eventReactive(input$run_bioclim_all_all_train,{
   if(!is.null(data_extraction())){
     if(!is.null(occ_extract()) && input$selectMBio == "wWorld" && input$trainBio == "wWorld"){
-      model_train <- dismo::bioclim(occ_extract()$data[,input$biosBioclim])
+      if(input$bpartition){
+        niche_data <- data_partition()[data_partition()$type=="train", ]
+      }
+      else{
+        niche_data <- occ_extract()$data
+      }
+      model_train <- dismo::bioclim(niche_data[,input$biosBioclim])
       model <- predict(rasterLayers()[[input$biosBioclim]], model_train)
       return(list(train=model_train,prediction=model))
     }
@@ -38,7 +59,13 @@ output$bio_response_all_all_train <- renderPlot({
 bioclim_model_all_m_train <- eventReactive(input$run_bioclim_all_m_train,{
   if(!is.null(data_extraction())){
     if(!is.null(occ_extract_from_mask()) && input$selectMBio == "wWorld" && input$trainBio == "mLayers"){
-      model_train <- bioclim(occ_extract_from_mask()$data[,input$biosBioclim])
+      if(input$bpartition){
+        niche_data <- data_partition()[data_partition()$type=="train", ]
+      }
+      else{
+        niche_data <- occ_extract_from_mask()$data
+      }
+      model_train <- bioclim(niche_data[,input$biosBioclim])
       model <- predict(rasterLayers()[[input$biosBioclim]], model_train)
       return(list(train=model_train,prediction=model))
     }
@@ -62,7 +89,13 @@ output$bio_response_all_m_train <- renderPlot({
 bioclim_model_m_all_train <- eventReactive(input$run_bioclim_m_all_train,{
   if(!is.null(data_extraction())){
     if(!is.null(occ_extract()) && input$selectMBio == "mLayers" && input$trainBio == "wWorld"){
-      model_train <- bioclim(occ_extract()$data[,input$biosBioclim])
+      if(input$bpartition){
+        niche_data <- data_partition()[data_partition()$type=="train", ]
+      }
+      else{
+        niche_data <- occ_extract()$data
+      }
+      model_train <- bioclim(niche_data[,input$biosBioclim])
       model <- predict(define_M_raster()[[input$biosBioclim]], model_train)
       return(list(train=model_train,prediction=model))
     }
@@ -76,7 +109,13 @@ bioclim_model_m_all_train <- eventReactive(input$run_bioclim_m_all_train,{
 bioclim_model_m_m_train <- eventReactive(input$run_bioclim_m_m_train,{
   if(!is.null(data_extraction())){
     if(!is.null(occ_extract_from_mask()) && input$selectMBio == "mLayers" && input$trainBio == "mLayers"){
-      model_train <- bioclim(occ_extract_from_mask()$data[,input$biosBioclim])
+      if(input$bpartition){
+        niche_data <- data_partition()[data_partition()$type=="train", ]
+      }
+      else{
+        niche_data <- occ_extract_from_mask()$data
+      }
+      model_train <- bioclim(niche_data[,input$biosBioclim])
       model <- predict(define_M_raster()[[input$biosBioclim]], model_train)
       return(list(train=model_train,prediction=model))
     }
