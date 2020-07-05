42eefa4d799f5d1f82c200133f188929912e55b7
Osorio Olvera <l916o895@home.ku.edu>
2020-07-05 15:23:51 -0500

    Create random partiton

--- inst/shinyApp/ui_layers/ui_MData.R
+++ inst/shinyApp/ui_layers/ui_MData.R
@@ -16,6 +16,16 @@ define_M <- sidebarLayout(position = "left",
                                           choices = c("Just clip my layers"="clipLayers")),
                               selectInput("extracted_area", "Select an area to make the extract",
                                           choices = c("All raster area"= "all_area", "Use the M polygon"="polygon_of_M")),
+                              checkboxInput("rpartition",
+                                            "Generate random partition for training and testing data",
+                                            value = TRUE),
+                              conditionalPanel("input.rpartition == true",
+                                               numericInput("rtestprop",
+                                                            "Train proportion",
+                                                            0.7,min = 0.01,
+                                                            max=0.99)
+                                               ),
+
                               actionButton("run_extract","Go!!!",styleclass = "primary"),
                               br(),
                               # fileInput('shapefolder', 'Upload the a zip with your shapefile'),
@@ -23,7 +33,9 @@ define_M <- sidebarLayout(position = "left",
                               # checkboxInput(inputId = "showTab", "Show data table",value = T),
                               downloadButton(outputId = "downloadExtraction",label = "Download data"),
                               busyIndicator("Loading..",wait = 0),
-                              conditionalPanel("input.extracted_area == 'polygon_of_M'", downloadButton(outputId = "downClippedR",label = "Clipped Raster"))
+                              conditionalPanel("input.extracted_area == 'polygon_of_M'",
+                                               downloadButton(outputId = "downClippedR",
+                                                              label = "Clipped Raster"))
 
                             )),
                           mainPanel(
