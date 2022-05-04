eb4e40360adca4eb69eb5f9705131bb7a7f00001
Luis Osorio <luismurao@gmail.com>
2022-05-04 18:15:19 -0500

    Update load_install_pkgs.R

--- inst/shinyApp/load_install_pkgs.R
+++ inst/shinyApp/load_install_pkgs.R
@@ -11,7 +11,7 @@ pkg_check <- c("shiny","rgeos","rgdal",
                "fields","devtools","psych",
                "magrittr","shinythemes","grid",
                "RColorBrewer","ade4","V8","shinyFiles",
-               "spocc","leaflet.extras","Rcpp","ntbox")
+               "spocc","leaflet.extras","Rcpp","ntbox","DT")
 
 pkgs_ntb_miss <- pkg_check[!(pkg_check %in% installed.packages())]
 ifelse("animation" %in% pkgs_ntb_miss,
